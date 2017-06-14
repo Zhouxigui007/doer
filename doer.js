32b2a04a08a51d3bf5d40000b96c272a761a306f
webproblem <yqx_emails@163.com>
2017-06-14 23:37:48 +0800

    update

--- doer.js
+++ doer.js
@@ -99,7 +99,169 @@
 				_elem.className = this.trim(cur);
 			}
 			return this;
-		}
+		}, 
+		//add action
+		css: function(attr, val) {
+            for (var i = 0; i < this.length; i++) {
+                if (typeof attr == 'string') {
+                    if (arguments.length == 1) {
+                        return getComputedStyle(this[i], null)[attr];
+                    }
+                    this[i].style[attr] = val;
+                } else {
+                    var self = this[i];
+                    Ye.each(attr, function(attr, val) {
+                        self.cssText += '' + attr + ':' + val + ';';
+                    });
+                }
+            }
+            return this;
+        },
+        attr: function(attr, val) {
+            for (var i = 0; i < this.length; i++) {
+                if (typeof attr == 'srting') {
+                    if (arguments.length == 1) {
+                        return this[i].getAttribute(attr);
+                    }
+                    this[i].setAttribute(attr, val);
+                } else {
+                    var self = this[i];
+                    f.each(attr, function(attr, val) {
+                        self.setAttribute(attr, val);
+                    });
+                }
+            }
+            return this;
+        },
+        data: function(attr, val) {
+            for (var i = 0; i < this.length; i++) {
+                if (typeof attr == 'string') {
+                    if (arguments.length == 1) {
+                        return this[i].getAttribute('data-' + attr);
+                    }
+                    this[i].setAttribute('data-' + attr, val);
+                } else {
+                    var self = this[i];
+                    f.each(attr, function(attr, val) {
+                        self.setAttribute('data-' + attr, val);
+                    });
+                }
+            }
+            return this;
+        },
+        next: function() {
+            return sibling(this[0], 'nextSibling');
+        },
+        prev: function() {
+            return sibling(this[0], 'previousSibling');
+        },
+		parent: function() {
+            var parent = this[0].parentNode;
+            parent && parent.nodeType !== 11 ? parent : null;
+            var a = Ye();
+            a[0] = parent;
+            a.length = 1;
+            a.selector = parent.tagName.toLowerCase();
+            return a;
+        },
+        parents: function() {
+            var a = Ye(),
+                i = 0;
+            while ((this[0] = this[0].parentNode) && this[0].nodeType !== 9) {
+                if (this[0].nodeType == 1) {
+                    a[i] = this[0];
+                    i++;
+                }
+            }
+            a.length = i;
+            console.log(a);
+            return a;
+        },
+        find: function(selector) {
+            if (!selector) return;
+            var context = this.selector;
+            return new Ye(context + ' ' + selector);
+        },
+        first: function() {
+            return new Ye(this[0]);
+        },
+        last: function() {
+            var num = this.length - 1;
+            return new Ye(this[num]);
+        },
+        eq: function(num) {
+            var num = num < 0 ? (this.length - 1) : num;
+            return new Ye(this[num]);
+        },
+        get: function(num) {
+            var num = num < 0 ? (this.length - 1) : num;
+            return this[num];
+        },
+        html: function(value) {
+            if (value === undefined) {
+                return this[0].innerHTML;
+            } else {
+                for (var i = 0; i < this.length; i++) {
+                    this[i].innerHTML = value;
+                }
+            }
+            return this;
+        },
+		append: function(str) {
+            for (var i = 0; i < this.length; i++) {
+                domAppend(this[i], 'beforeend', str);
+            }
+            return this;
+        },
+        before: function(str) {
+            for (var i = 0; i < this.length; i++) {
+                domAppend(this[i], 'beforeBegin', str);
+            }
+            return this;
+        },
+        after: function(str) {
+            for (var i = 0; i < this.length; i++) {
+                domAppend(this[i], 'afterend', str);
+            }
+            return this;
+        },
+        remove: function() {
+            //删除自身
+            for (var i = 0; i < this.length; i++) {
+                this[i].parentNode.removeChild(this[i]);
+            }
+        },
+		width: function(w) {
+            if (arguments.length == 1) {
+                for (var i = 0; i < this.length; i++) {
+                    this[i].style.width = w + 'px';
+                }
+            } else {
+                if (this[0].document = doc) {
+                    return this[0].innerWidth;
+                } else if (this[0].nodeType == 9) {
+                    return document.documentElement.clientWidth;
+                } else {
+                    return parseInt(getComputedStyle(this[0], null)['width']);
+                }
+            }
+        },
+        height: function(h) {
+            if (arguments.length == 1) {
+                for (var i = 0; i < this.length; i++) {
+                    this[i].style.height = w + 'px';
+                }
+            } else {
+                if (this[0].document = doc) {
+                    return this[0].innerheight;
+                } else if (this[0].nodeType == 9) {
+                    return document.documentElement.clientheight;
+                } else {
+                    return parseInt(getComputedStyle(this[0], null)['height']);
+                }
+            }
+        }
+		//add end
 	};
 	Doer.prototype.init.prototype = Doer.prototype;
 	win.$d = Doer;
